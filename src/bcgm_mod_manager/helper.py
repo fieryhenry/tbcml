46125bb62ef4bfd00b5b8b77052cb3e8ab808f4e
fieryhenry <git@henrythomas.me.uk>
2022-08-28 18:23:26 +0100

    Added stuff with tk for game file modding

--- src/bcgm_mod_manager/helper.py
+++ src/bcgm_mod_manager/helper.py
@@ -5,12 +5,38 @@ from typing import Any, Optional
 import zipfile
 from Cryptodome.Cipher import AES
 import hashlib
-import yaml
 import colored  # type: ignore
 
 from tkinter import filedialog, Tk
 import requests
 
+def get_files_in_dir(dir_path: str) -> list[str]:
+    """
+    Get all files in a directory
+
+    Args:
+        dir_path (str): Path to directory to get files from
+
+    Returns:
+        list[str]: Paths to files in directory
+    """
+    return [
+        os.path.join(dir_path, file) for file in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, file))
+    ]
+
+def get_folders_in_dir(dir_path: str) -> list[str]:
+    """
+    Get all folders in a directory
+
+    Args:
+        dir_path (str): Path to directory to get folders from
+
+    Returns:
+        list[str]: Paths to folders in directory
+    """
+    return [
+        os.path.join(dir_path, folder) for folder in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, folder))
+    ]
 
 class Color(Enum):
     GREEN = "#008000"
@@ -54,6 +80,99 @@ def colored_input(
     return input()
 
 
+def init_tk() -> Tk:
+    """
+    Initialize a Tkinter window
+
+    Returns:
+        Tk: Tkinter window
+    """
+    root = Tk()
+    root.withdraw()
+    root.wm_attributes("-topmost", 1)  # type: ignore
+    return root
+
+
+def select_file(
+    title: str,
+    file_types: list[tuple[str, str]] = [("All Files", "*")],
+    initial_dir: str = ".",
+) -> str:
+    """
+    Select a file to open
+
+    Args:
+        title (str): Title of dialog
+        file_types (list[tuple[str, str]], optional): File types to allow. Defaults to [("All Files", "*")].
+        initial_dir (str, optional): Initial directory to open in. Defaults to ".".
+
+    Returns:
+        str: Path to file selected
+    """
+    root = init_tk()
+    file = filedialog.askopenfilename(
+        title=title,
+        filetypes=file_types,
+        initialdir=initial_dir,
+    )
+    root.destroy()
+    if file is None:
+        return ""
+    return file
+
+
+def get_vals_from_user(options: list[str]) -> list[str]:
+    """
+    Get values from the user for a list of options
+
+    Args:
+        options (list[str]): Options to get values for
+
+    Returns:
+        list[str]: Values from user
+    """
+    values: list[str] = []
+    for option in options:
+        value = colored_input(option)
+        values.append(value)
+    return values
+
+
+def extend_list(list1: list[Any], list2: list[Any]) -> list[Any]:
+    """
+    Extend a list with 0s if it is shorter than another list
+
+    Args:
+        list1 (list[Any]): List to extend
+        list2 (list[Any]): Length of list to extend with
+
+    Returns:
+        list[Any]: Extended list
+    """
+
+    if len(list1) < len(list2):
+        for _ in range(len(list2) - len(list1)):
+            list1.append(0)
+    return list1
+
+
+def int_to_str_ls(int_list: list[int]) -> list[str]:
+    """
+    Convert a list of integers to a list of strings
+
+    Args:
+        int_list (list[int]): List of integers to convert to strings
+
+    Returns:
+        list[str]: List of strings
+    """
+
+    str_list: list[str] = []
+    for i in int_list:
+        str_list.append(str(i))
+    return str_list
+
+
 def select_files(
     title: str,
     file_types: list[tuple[str, str]] = [("All Files", "*")],
@@ -70,9 +189,7 @@ def select_files(
     Returns:
         list[str]: Paths to files selected
     """
-    root = Tk()
-    root.withdraw()
-    root.wm_attributes("-topmost", 1)  # type: ignore
+    root = init_tk()
     files = filedialog.askopenfilenames(
         title=title,
         filetypes=file_types,
@@ -238,6 +355,70 @@ def list_to_csv(list_of_lists: list[list[Any]], delimiter: str = ",") -> str:
     return csv
 
 
+def get_elements(list_of_lists: list[list[Any]], n: int) -> list[Any]:
+    """
+    Get the first n elements of a list of lists
+
+    Args:
+        list_of_lists (list[list[Any]]): List of lists to get elements from
+        n (int): Number of elements to get
+
+    Returns:
+        list[Any]: First n elements of list of lists
+    """
+
+    elements: list[Any] = []
+    for ls in list_of_lists:
+        elements.append(ls[n])
+    return elements
+
+
+def select_dir(title: str, default_dir: str) -> str:
+    """
+    Select a directory from the user
+
+    Args:
+        title (str): Title of the dialog
+        default_dir (str): Default directory to select
+
+    Returns:
+        str: Selected directory
+    """
+    root = init_tk()
+    dir_path = filedialog.askdirectory(
+        title=title, initialdir=default_dir, parent=root
+    )
+    return dir_path
+
+def str_to_chars(string: str) -> list[str]:
+    """
+    Convert a string to a list of characters
+
+    Args:
+        string (str): String to convert to list of characters
+
+    Returns:
+        list[str]: List of characters
+    """
+    return list(string)
+
+
+def int_list_to_str(ls: list[int]) -> str:
+    """
+    Convert a list of ints to a string
+
+    Args:
+        ls (list[int]): List of ints to convert to string
+
+    Returns:
+        str: String of ints
+    """
+    string = ""
+    for i in ls:
+        string += str(i)
+    return string
+
+
 def check_dir(dir_path: str) -> None:
     """
     Check if a directory exists and create it if it doesn't
@@ -247,6 +428,7 @@ def check_dir(dir_path: str) -> None:
     """
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
+
 
 def download_file(url: str, file_path: str, headers: dict[str, Any], percentage: bool):
     """
@@ -272,6 +454,7 @@ def download_file(url: str, file_path: str, headers: dict[str, Any], percentage:
                 print("\rDownloaded: {}%".format(int(100 * wrote / total_size)), end="")
     print("\n")
 
+
 def unzip_file(file_path: str, destination: str) -> None:
     """
     Unzip a file to a destination
@@ -282,6 +465,7 @@ def unzip_file(file_path: str, destination: str) -> None:
     """
     with zipfile.ZipFile(file_path, "r") as zip_ref:
         zip_ref.extractall(destination)
+
 
 def parse_csv(
     file_path: Optional[str] = None,
@@ -459,7 +643,7 @@ def gv_to_str(game_version: int) -> str:
 
     Returns:
         str: Game version with semantic versioning
-    """    
+    """
 
     split_gv = str(game_version).zfill(6)
     split_gv = [str(int(split_gv[i : i + 2])) for i in range(0, len(split_gv), 2)]
@@ -562,32 +746,6 @@ def get_int(value: str) -> Optional[int]:
         return int(value)
     except ValueError:
         return None
-
-
-def load_config() -> dict[str, Any]:
-    """
-    Load the config file
-
-    Returns:
-        dict[str, Any]: Config file
-    """
-    with open(get_file("config.yaml"), "r") as f:
-        config = yaml.safe_load(f)
-    return config
-
-
-def get_config_value(key: str) -> Any:
-    """
-    Get a config value
-
-    Args:
-        key (str): Key to get the value of
-
-    Returns:
-        Any: Config value
-    """
-    config = load_config()
-    return config[key]
 
 
 def get_files_path() -> str:
