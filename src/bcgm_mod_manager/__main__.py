c3fff7f6bd97a58c35b2f50a8406c6a2ba4cd4ac
fieryhenry <git@henrythomas.me.uk>
2022-09-05 08:28:49 +0100

    Moved features to feature handler

--- src/bcgm_mod_manager/__main__.py
+++ src/bcgm_mod_manager/__main__.py
@@ -1,447 +1,49 @@
-import os
-from typing import Any
-
-from . import apk_handler, config_handler, game_file_edits, helper, mod, mod_manager
-
-
-def download_server_packs():
-    is_jp = (
-        helper.colored_input(
-            "Do you want to get the jp version of the game files? (&y&/&n&):"
-        )
-        == "y"
-    )
-    apk_handler.download_server_files(is_jp)
-
-
-def add_file_to_mod() -> None:
-    """
-    Adds a file to a mod.
-    """
-    mods = mod_manager.get_enabled_mods()
-    if not mods:
-        helper.colored_text("No mods enabled.", helper.Color.RED)
-        return
-    helper.colored_list(
-        list(map(lambda x: mod_manager.get_mod_name(x), mods)), new=helper.Color.GREEN
-    )
-    choice = helper.get_int(input("Enter an option:"))
-    if choice is None:
-        helper.colored_text("Invalid choice.", helper.Color.RED)
-        helper.colored_text("Please try again.", helper.Color.RED)
-        return
-    if choice > 0 and choice <= len(mods):
-        mod_name = mod_manager.get_mod_name(mods[choice - 1])
-        helper.colored_text("Adding files to mod: " + mod_name, helper.Color.GREEN)
-        mod_manager.add_files_to_mod(mod_name)
-    else:
-        helper.colored_text("Invalid choice.", helper.Color.RED)
-        helper.colored_text("Please try again.", helper.Color.RED)
-
-
-def extract_all_mods() -> None:
-    """
-    Extracts all mods.
-    """
-    mods = mod_manager.load_mods()
-    path = os.path.join(mod_manager.get_mod_folder(), "unpacked_mods")
-    for mod in mods:
-        mod_path = os.path.join(path, mod_manager.get_mod_name(mod))
-        mod.unpack(mod_path)
-    helper.colored_text("Mods extracted successfully.", helper.Color.GREEN)
-    if (
-        helper.colored_input(
-            "Do you want to open the folder of the content? (&y&/&n&):"
-        )
-        == "y"
-    ):
-        os.startfile(path)
-
-
-def extract_mod_pack() -> None:
-    """
-    Extracts a mod pack.
-    """
-    mod_packs = mod_manager.get_mod_packs()
-    if not mod_packs:
-        helper.colored_text("No mod_packs found.", helper.Color.RED)
-        return
-    helper.colored_list(list(map(lambda x: mod_manager.get_mod_name(x), mod_packs)))
-    choice = helper.get_int(input("Enter an option:"))
-    if choice is None:
-        helper.colored_text("Invalid choice.", helper.Color.RED)
-        helper.colored_text("Please try again.", helper.Color.RED)
-        return
-    if choice > 0 and choice <= len(mod_packs):
-        mod_pack = mod_packs[choice - 1]
-        helper.colored_text(
-            "Extracting mod pack: " + mod_pack.get_name(), helper.Color.GREEN
-        )
-        mod_manager.extract_mod_pack(mod_pack)
-    else:
-        helper.colored_text("Invalid choice.", helper.Color.RED)
-        helper.colored_text("Please try again.", helper.Color.RED)
-
-
-def decrypt_all_game_files() -> None:
-    """
-    Decrypts all game files.
-    """
-    is_jp = (
-        helper.colored_input(
-            "Do you want to get the jp version of the game files? (&y&/&n&):"
-        )
-        == "y"
-    )
-    helper.colored_text("Loading apk...", helper.Color.GREEN)
-    apk = apk_handler.BC_APK("latest", is_jp, apk_handler.BC_APK.get_apk_folder())
-    apk.download()
-    helper.colored_text("Extracting apk...", helper.Color.GREEN)
-    apk.extract()
-    helper.colored_text("Decrypting game files...", helper.Color.GREEN)
-    apk.decrypt()
-
-    if (
-        helper.colored_input(
-            "Do you want to open the decrypted files folder? (&y&/&n&):"
-        )
-        == "y"
-    ):
-        os.startfile(apk.decrypted_path)
-
-
-def open_mod_folder() -> None:
-    """
-    Opens the mod folder.
-    """
-    os.startfile(mod_manager.get_mod_folder())
-
-
-def enable_mods() -> None:
-    """
-    Enables mods.
-    """
-    mods = mod_manager.get_disabled_mods()
-    if not mods:
-        helper.colored_text("No mods disabled.", helper.Color.RED)
-        return
-    helper.colored_list(
-        list(map(lambda x: mod_manager.get_mod_name(x), mods)), new=helper.Color.GREEN
-    )
-    choices = helper.colored_input(
-        "\nEnter an option (You can enter multiple numbers separated by spaces to select multiple at once):"
-    ).split(" ")
-    for choice in choices:
-        if choice is None:
-            helper.colored_text("Invalid choice.", helper.Color.RED)
-            helper.colored_text("Please try again.", helper.Color.RED)
-            return
-        choice = helper.get_int(choice)
-        if choice is None:
-            helper.colored_text("Invalid choice.", helper.Color.RED)
-            helper.colored_text("Please try again.", helper.Color.RED)
-            return
-        if choice > 0 and choice <= len(mods):
-            helper.colored_text(
-                "Enabling mod: " + mod_manager.get_mod_name(mods[choice - 1]),
-                helper.Color.GREEN,
-            )
-            mod_manager.enable_mod(mod_manager.get_mod_name(mods[choice - 1]))
-
-
-def disable_mods() -> None:
-    """
-    Disables mods.
-    """
-    mods = mod_manager.get_enabled_mods()
-    if not mods:
-        helper.colored_text("No mods enabled.", helper.Color.RED)
-        return
-    helper.colored_list(
-        list(map(lambda x: mod_manager.get_mod_name(x), mods)), new=helper.Color.GREEN
-    )
-    choices = helper.colored_input(
-        "\nEnter an option (You can enter multiple numbers separated by spaces to select multiple at once):"
-    ).split(" ")
-    for choice in choices:
-        if choice is None:
-            helper.colored_text("Invalid choice.", helper.Color.RED)
-            helper.colored_text("Please try again.", helper.Color.RED)
-            return
-        choice = helper.get_int(choice)
-        if choice is None:
-            helper.colored_text("Invalid choice.", helper.Color.RED)
-            helper.colored_text("Please try again.", helper.Color.RED)
-            return
-        if choice > 0 and choice <= len(mods):
-            helper.colored_text(
-                "Disabling mod: " + mod_manager.get_mod_name(mods[choice - 1]),
-                helper.Color.GREEN,
-            )
-            mod_manager.disable_mod(mod_manager.get_mod_name(mods[choice - 1]))
-
-
-def add_mods() -> None:
-    """
-    Adds mods to the mod list.
-    """
-    files = helper.select_files(
-        title="Select mods to add", file_types=[("Mod", mod.Mod.get_extension())]
-    )
-    for file in files:
-        bc_mod = mod.Mod.load_from_mod_file(file)
-        mod_manager.add_mod(bc_mod)
-    helper.colored_text("Mods added successfully.", helper.Color.GREEN)
-
-
-def remove_mods() -> None:
-    """
-    Removes mods from the mod list.
-    """
-    mods = mod_manager.load_mods()
-    if not mods:
-        helper.colored_text("No mods found.", helper.Color.RED)
-        return
-    helper.colored_list(
-        list(map(lambda x: mod_manager.get_mod_name(x), mods)), new=helper.Color.GREEN
-    )
-    choices = helper.colored_input(
-        "\nEnter an option (You can enter multiple numbers separated by spaces to select multiple at once):"
-    ).split(" ")
-    for choice in choices:
-        if choice is None:
-            helper.colored_text("Invalid choice.", helper.Color.RED)
-            helper.colored_text("Please try again.", helper.Color.RED)
-            return
-        choice = helper.get_int(choice)
-        if choice is None:
-            helper.colored_text("Invalid choice.", helper.Color.RED)
-            helper.colored_text("Please try again.", helper.Color.RED)
-            return
-        if choice > 0 and choice <= len(mods):
-            helper.colored_text(
-                "Removing mod: " + mod_manager.get_mod_name(mods[choice - 1]),
-                helper.Color.GREEN,
-            )
-            mod_manager.remove_mod(mod_manager.get_mod_name(mods[choice - 1]))
-
-
-def add_mod_packs() -> None:
-    """
-    Adds mod packs to the mod list.
-    """
-    files = helper.select_files(
-        title="Select mod packs to add",
-        file_types=[("Mod pack", mod.ModPack.get_extension())],
-    )
-    for file in files:
-        bc_mod_pack = mod.ModPack.load_from_mod_pack(file)
-        mod_manager.add_modpack(bc_mod_pack)
-    helper.colored_text("Mod packs added successfully.", helper.Color.GREEN)
-
-
-def create_mod_pack() -> None:
-    """
-    Creates a mod pack.
-    """
-    name = helper.colored_input("Enter the name of the mod pack:")
-    author = helper.colored_input("Enter the author of the mod pack:")
-    description = helper.colored_input("Enter the description of the mod pack:")
-    game_version = helper.colored_input(
-        "Enter the game version of the mod pack (e.g 11.7.1):"
-    )
-    is_jp = helper.colored_input("Is this for the jp version? (&y&/&n&):") == "y"
-
-    mod_manager.create_mod_pack(
-        name, author, description, helper.str_to_gv(game_version), is_jp
-    )
-
-
-def create_mod() -> None:
-    """
-    Creates a mod.
-    """
-    name = helper.colored_input("Enter the name of the mod:")
-    author = helper.colored_input("Enter the author of the mod:")
-    description = helper.colored_input("Enter the description of the mod:")
-    game_version = helper.colored_input(
-        "Enter the game version of the mod (e.g 11.7.1):"
-    )
-    country_code = helper.colored_input(
-        "Enter the country code of the mod (e.g en, jp, kr, tw):"
-    )
-    cc = "jp" if country_code == "jp" else ""
-    files = helper.select_files(
-        title="Select files to add to the mod",
-        initial_dir=os.path.join(
-            config_handler.get_config_setting("apk_folder"),
-            game_version + cc,
-            "Modified Files",
-        ),
-    )
-
-    mod_manager.create_mod(
-        name=name,
-        author=author,
-        description=description,
-        game_version=game_version,
-        country_code=country_code,
-        files=files,
-    )
-    helper.colored_text("Mod created successfully.", helper.Color.GREEN)
-
-
-def exit_manager() -> None:
-    """
-    Exits the mod manager.
-    """
-    helper.colored_text("\nExiting modder.", helper.Color.GREEN)
-    exit()
-
-
-def load_mods_into_game() -> None:
-    """
-    Loads mods into the game.
-    """
-    game_version = mod_manager.get_newest_mod_version()
-    if game_version is None:
-        helper.colored_text("No mods found.", helper.Color.RED)
-        return
-    is_jp = helper.colored_input("Are you using a jp version? (&y&/&n&):") == "y"
-
-    if not mod_manager.load_mods_into_game(helper.gv_to_str(game_version), is_jp):
-        helper.colored_text("\nFailed to load mods into the apk.", helper.Color.RED)
-        return
-
-    helper.colored_text("\nSuccessfully loaded mods into the apk.", helper.Color.GREEN)
-    apk_path = os.path.abspath(
-        os.path.join(
-            apk_handler.BC_APK.get_apk_folder(),
-            helper.gv_to_str(game_version),
-            "modded.apk",
-        )
-    )
-    helper.colored_text(
-        f"\n&The apk can be found here: &{apk_path}",
-        helper.Color.GREEN,
-        helper.Color.WHITE,
-    )
-    if (
-        helper.colored_input(
-            "Would you like to open the folder containing the apk? (&y&/&n&):"
-        )
-        == "y"
-    ):
-        os.startfile(os.path.dirname(apk_path))
-    helper.colored_text("Please re-install the game", helper.Color.GREEN)
-
-
-OPTIONS: dict[str, Any] = {
-    "Display mods": mod_manager.display_mods,
-    "Load enabled mods into apk": load_mods_into_game,
-    "Mod Management": {
-        "Enable mods": enable_mods,
-        "Disable mods": disable_mods,
-        "Add game files to mod": add_file_to_mod,
-        "Remove mods": remove_mods,
-        "Load mods from .bcmod files": add_mods,
-        "Create mod from game files": create_mod,
-        "Load enabled mods into apk": load_mods_into_game,
-        "Open mods folder in explorer": open_mod_folder,
-    },
-    "Data Decryption / Extraction / Download": {
-        "Extract all mods into game files": extract_all_mods,
-        "Extract mod pack into mod files": extract_mod_pack,
-        "Decrypt all game files": decrypt_all_game_files,
-        "Download server pack files": download_server_packs,
-    },
-    "Mod Pack Management": {
-        "Load mod packs from .bcmodpack files": add_mod_packs,
-        "Create mod pack of enabled mods": create_mod_pack,
-    },
-    "Edit Game Files": {
-        "Edit cat stats (unit*.csv)": game_file_edits.unit_mod.edit_unit,
-        "Edit stage data (stage*.csv)": game_file_edits.stage_mod.edit_stage,
-        "Edit enemy data (t_unit.csv)": game_file_edits.enemy_mod.edit_enemy,
-        "Import bcu data": game_file_edits.import_from_bcu.import_from_bcu,
-        "Add enemies as cats": game_file_edits.add_enemy_as_cat.import_enemy,
-    },
-    "Set Config Settings": {
-        "Set apk folder": config_handler.set_apk_folder,
-        "Set mod folder": config_handler.set_mod_folder,
-        "Set apk copy path": config_handler.set_apk_copy_path,
-    },
-    "Exit": exit_manager,
-}
-
-
-def get_feature(
-    selected_features: Any, search_string: str, results: dict[str, Any]
-) -> dict[str, Any]:
-    """Search for a feature if the feature name contains the search string"""
-
-    for feature in selected_features:
-        feature_data = selected_features[feature]
-        if isinstance(feature_data, dict):
-            feature_data = get_feature(feature_data, search_string, results)
-        if search_string.lower().replace(" ", "") in feature.lower().replace(" ", ""):
-            results[feature] = selected_features[feature]
-    return results
-
-
-def show_options(features_to_use: dict[str, Any]) -> None:
-    """Allow the user to either enter a feature number or a feature name, and get the features that match"""
-
-    prompt = "What do you want to do?(some options contain other features within them)"
-    prompt += "\nYou can enter a number to run a feature or a word to search for that feature (e.g entering enable mod will run the Enable Mods feature)\nYou can press enter to see a list of all of the features"
-    user_input = helper.colored_input(f"{prompt}:\n")
-    user_int = helper.get_int(user_input)
-    results = []
-    if user_int is None:
-        results = get_feature(features_to_use, user_input, {})
-    else:
-        if user_int < 1 or user_int > len(features_to_use) + 1:
-            helper.colored_text("Value out of range", helper.Color.RED)
-            return show_options(features_to_use)
-        if OPTIONS != features_to_use:
-            if user_int - 2 < 0:
-                return menu()
-            results = features_to_use[list(features_to_use)[user_int - 2]]
-        else:
-            results = features_to_use[list(features_to_use)[user_int - 1]]
-    if not isinstance(results, dict):
-        return results()
-    if len(results) == 0:
-        helper.colored_text("No feature found with that name.", helper.Color.RED)
-        return menu()
-    if len(results) == 1 and isinstance(list(results.values())[0], dict):
-        results = results[list(results)[0]]
-    if len(results) == 1:
-        return results[list(results)[0]]()
-
-    helper.colored_list(["Go Back"] + list(results))
-    return show_options(results)
-
+from . import feature_handler
+import argparse
 
 def main() -> None:
     """
     Main function.
     """
-    menu()
+    args = parse_args()
+    handle_args(args)
+    feature_handler.menu()
 
 
-def menu() -> None:
+def parse_args() -> argparse.Namespace:
     """
-    The main menu.
+    Parse command line arguments.
+
+    Returns:
+        argparse.Namespace: Parsed arguments.
+    """    
+    parser = argparse.ArgumentParser(description="BCGM Mod Manager")
+    parser.add_argument("-d", "--decrypt", action="store_true", help="Decrypt the game files", default=False)
+    parser.add_argument("-f", "--files", help="List of files to decrypt", nargs="+", type=str, default=[])
+    parser.add_argument("-v", "--version", type=str, help="Version of the game to decrypt", default="latest")
+    parser.add_argument("-j", "--jp", action="store_true", help="Decrypt jp files", default=False)
+    parser.add_argument("-o", "--output", type=str, help="Output directory", default="output")
+    parser.add_argument("-e", "--exit", action="store_true", help="Exit after running", default=False)
+    return parser.parse_args()
+
+def handle_args(args: argparse.Namespace) -> None:
     """
-    while True:
-        helper.colored_text("\nMod Manager", helper.Color.GREEN)
-        helper.colored_list(list(OPTIONS))
-        show_options(OPTIONS)
+    Handle command line arguments.
+
+    Args:
+        args (argparse.Namespace): Parsed arguments.
+    """
+    if args.decrypt:
+        if args.files and args.version:
+            feature_handler.decrypt_files(args.files, args.version, args.jp, args.output)
+        else:
+            print("You need to specify a list of files and a version to decrypt")
+    if args.exit:
+        exit()
 
 
 if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        exit_manager()
+        feature_handler.exit_manager()
