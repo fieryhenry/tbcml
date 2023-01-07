3da169bd4d73f49bb9c3819cf4edd18570b607df
fieryhenry <github@henrythomas.me.uk>
2023-01-07 17:54:19 +0000

    Added inapp and scheme items

--- src/bcml/core/game_data/cat_base/__init__.py
+++ src/bcml/core/game_data/cat_base/__init__.py
@@ -1,3 +1,21 @@
-from bcml.core.game_data.cat_base import cat_base, cats, enemies, gatya, unit, item_shop
+from bcml.core.game_data.cat_base import (
+    cat_base,
+    cats,
+    enemies,
+    gatya,
+    unit,
+    item_shop,
+    nekokan_product,
+    scheme_item,
+)
 
-__all__ = ["cat_base", "cats", "enemies", "gatya", "unit", "item_shop"]
+__all__ = [
+    "cat_base",
+    "cats",
+    "enemies",
+    "gatya",
+    "unit",
+    "item_shop",
+    "nekokan_product",
+    "scheme_item",
+]
--- src/bcml/core/game_data/cat_base/cat_base.py
+++ src/bcml/core/game_data/cat_base/cat_base.py
@@ -1,5 +1,12 @@
 from typing import Any, Optional
-from bcml.core.game_data.cat_base import cats, enemies, gatya, item_shop
+from bcml.core.game_data.cat_base import (
+    cats,
+    enemies,
+    gatya,
+    item_shop,
+    nekokan_product,
+    scheme_item,
+)
 from bcml.core.game_data import pack
 from bcml.core import io
 
@@ -11,11 +18,15 @@ class CatBase:
         enemies: "enemies.Enemies",
         gatya: "gatya.Gatya",
         item_shop: "item_shop.ItemShop",
+        nekokan_product: "nekokan_product.NekokanProducts",
+        scheme_items: "scheme_item.SchemeItems",
     ):
         self.cats = cats
         self.enemies = enemies
         self.gatya = gatya
         self.item_shop = item_shop
+        self.nekokan_product = nekokan_product
+        self.scheme_items = scheme_items
 
     def serialize(self) -> dict[str, Any]:
         return {
@@ -23,6 +34,8 @@ class CatBase:
             "enemies": self.enemies.serialize(),
             "gatya": self.gatya.serialize(),
             "item_shop": self.item_shop.serialize(),
+            "nekokan_product": self.nekokan_product.serialize(),
+            "scheme_items": self.scheme_items.serialize(),
         }
 
     @staticmethod
@@ -32,6 +45,8 @@ class CatBase:
             enemies.Enemies.deserialize(data["enemies"]),
             gatya.Gatya.deserialize(data["gatya"]),
             item_shop.ItemShop.deserialize(data["item_shop"]),
+            nekokan_product.NekokanProducts.deserialize(data["nekokan_product"]),
+            scheme_item.SchemeItems.deserialize(data["scheme_items"]),
         )
 
     @staticmethod
@@ -40,13 +55,17 @@ class CatBase:
         enem = enemies.Enemies.from_game_data(game_data)
         gt = gatya.Gatya.from_game_data(game_data)
         itms = item_shop.ItemShop.from_game_data(game_data)
-        if cts is None or enem is None or gt is None or itms is None:
+        np = nekokan_product.NekokanProducts.from_game_data(game_data)
+        sch = scheme_item.SchemeItems.from_game_data(game_data)
+        if cts is None or enem is None or gt is None or itms is None or np is None or sch is None:
             return None
         return CatBase(
             cts,
             enem,
             gt,
             itms,
+            np,
+            sch,
         )
 
     def to_game_data(self, game_data: "pack.GamePacks"):
@@ -54,12 +73,16 @@ class CatBase:
         self.enemies.to_game_data(game_data)
         self.gatya.to_game_data(game_data)
         self.item_shop.to_game_data(game_data)
+        self.nekokan_product.to_game_data(game_data)
+        self.scheme_items.to_game_data(game_data)
 
     def add_to_zip(self, zip: "io.zip.Zip"):
         self.cats.add_to_zip(zip)
         self.enemies.add_to_zip(zip)
         self.gatya.add_to_zip(zip)
         self.item_shop.add_to_zip(zip)
+        self.nekokan_product.add_to_zip(zip)
+        self.scheme_items.add_to_zip(zip)
 
     @staticmethod
     def from_zip(zip: "io.zip.Zip") -> Optional["CatBase"]:
@@ -67,13 +90,17 @@ class CatBase:
         enem = enemies.Enemies.from_zip(zip)
         gt = gatya.Gatya.from_zip(zip)
         itms = item_shop.ItemShop.from_zip(zip)
-        if cts is None or enem is None or gt is None or itms is None:
+        np = nekokan_product.NekokanProducts.from_zip(zip)
+        sch = scheme_item.SchemeItems.from_zip(zip)
+        if cts is None or enem is None or gt is None or itms is None or np is None or sch is None:
             return None
         return CatBase(
             cts,
             enem,
             gt,
             itms,
+            np,
+            sch,
         )
 
     @staticmethod
@@ -83,10 +110,14 @@ class CatBase:
             enemies.Enemies.create_empty(),
             gatya.Gatya.create_empty(),
             item_shop.ItemShop.create_empty(),
+            nekokan_product.NekokanProducts.create_empty(),
+            scheme_item.SchemeItems.create_empty(),
         )
-    
+
     def import_cat_base(self, other: "CatBase"):
         self.cats.import_cats(other.cats)
         self.enemies.import_enemies(other.enemies)
         self.gatya.import_gatya(other.gatya)
         self.item_shop.import_item_shop(other.item_shop)
+        self.nekokan_product.import_nekokan(other.nekokan_product)
+        self.scheme_items.import_scheme_items(other.scheme_items)
