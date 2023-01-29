d0d15469ddbcb117d456669fb1b3d5481f112245
fieryhenry <github@henrythomas.me.uk>
2023-01-29 16:04:23 +0000

    Moved file selection to new file

--- src/bcml/ui/__init__.py
+++ src/bcml/ui/__init__.py
@@ -1,3 +1,10 @@
-from bcml.ui import main, apk_manager, progress, ui_thread, ui_dialog
+from bcml.ui import main, apk_manager, progress, ui_thread, ui_dialog, ui_file_dialog
 
-__all__ = ["main", "apk_manager", "progress", "ui_thread", "ui_dialog"]
+__all__ = [
+    "main",
+    "apk_manager",
+    "progress",
+    "ui_thread",
+    "ui_dialog",
+    "ui_file_dialog",
+]
