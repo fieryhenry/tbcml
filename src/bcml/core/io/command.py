e9903265aa1b986642ae54b53958094d03fc54d5
fieryhenry <github@henrythomas.me.uk>
2023-01-29 10:09:15 +0000

    Capture output

--- src/bcml/core/io/command.py
+++ src/bcml/core/io/command.py
@@ -1,21 +1,23 @@
 import subprocess
 
+
 class Result:
     def __init__(self, result: str, exit_code: int):
         self.result = result
         self.exit_code = exit_code
-    
+
     def __str__(self) -> str:
         return self.result
-    
+
     def __repr__(self) -> str:
         return f"Result({self.result!r}, {self.exit_code!r})"
+
 
 class Command:
     def __init__(self, command: str, display_output: bool = True):
         self.command = command
         self.display_output = display_output
-    
+
     def run(self, inputData: str = "\n") -> Result:
         self.process = subprocess.Popen(
             self.command,
@@ -25,9 +27,19 @@ class Command:
             shell=True,
             universal_newlines=True,
         )
-        if self.display_output and self.process.stdout:
-            for line in self.process.stdout:
-                print(line, end="")
-        output = self.process.communicate(inputData)[0]
-        exit_code = self.process.wait()
-        return Result(output, exit_code)
+        # get output
+        output = ""
+        while True:
+            if self.process.stdout is None:
+                break
+            line = self.process.stdout.readline()
+            if line != "":
+                output += line
+                if self.display_output:
+                    print(line, end="")
+            else:
+                break
+        # get return code
+        self.process.communicate(inputData)
+        return_code = self.process.wait()
+        return Result(output, return_code)
