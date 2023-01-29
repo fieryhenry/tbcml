0875f03e711b6964e7c3b1b1c2ca261acf6b32f9
fieryhenry <github@henrythomas.me.uk>
2023-01-29 15:37:29 +0000

    Added logic to select apk + progress bar

--- src/bcml/ui/apk_manager.py
+++ src/bcml/ui/apk_manager.py
@@ -3,7 +3,7 @@ from typing import Any, Callable, Optional
 import webbrowser
 from PyQt5 import QtCore, QtWidgets, QtGui
 from bcml.core import io, country_code, game_version
-from bcml.ui import main
+from bcml.ui import main, progress, ui_thread, ui_dialog
 
 
 class ApkManager(QtWidgets.QDialog):
@@ -27,7 +27,21 @@ class ApkManager(QtWidgets.QDialog):
 
     def add_ui(self):
         io.apk.Apk.clean_up()
-        self.apk_list = ApkList(self)
+
+        self.selected_apk_label = QtWidgets.QLabel("Selected APK:")
+        self._layout.addWidget(self.selected_apk_label)
+
+        self.selected_apk = io.config.Config().get(io.config.Key.SELECTED_APK)
+        self.selected_apk_label = QtWidgets.QLineEdit()
+        self.selected_apk_label.setReadOnly(True)
+        self.selected_apk_label.setPlaceholderText("None")
+        if self.selected_apk:
+            self.selected_apk_label.setText(self.selected_apk)
+        else:
+            self.selected_apk_label.setText("None")
+        self._layout.addWidget(self.selected_apk_label)
+
+        self.apk_list = ApkList(self.select_apk, self)
         self.apk_list.get_apks()
         self._layout.addWidget(self.apk_list)
 
@@ -84,10 +98,19 @@ class ApkManager(QtWidgets.QDialog):
             apk = io.apk.Apk.from_apk_path(path)
             self.add_apk_call(apk)
 
+    def select_apk(self, apk: io.apk.Apk):
+        io.config.Config().set(io.config.Key.SELECTED_APK, apk.format())
+        self.selected_apk_label.setText(apk.format())
+
 
 class ApkList(QtWidgets.QListWidget):
-    def __init__(self, parent: Optional[QtWidgets.QWidget] = None):
+    def __init__(
+        self,
+        on_select_apk: Callable[[io.apk.Apk], None],
+        parent: Optional[QtWidgets.QWidget] = None,
+    ):
         super(ApkList, self).__init__(parent)
+        self.on_select_apk = on_select_apk
         self.setup_ui()
 
     def setup_ui(self):
@@ -102,11 +125,14 @@ class ApkList(QtWidgets.QListWidget):
             return
         delete_action = menu.addAction("Delete APK")
         extraction_action = menu.addAction("Extract APK")
+        select_apk_action = menu.addAction("Select APK")
         action = menu.exec_(self.mapToGlobal(pos))
         if action == delete_action:
             self.delete_apk()
         elif action == extraction_action:
             self.extract_apk()
+        elif action == select_apk_action:
+            self.select_apk()
 
     def get_apks(self):
         self.apks = io.apk.Apk.get_all_downloaded()
@@ -121,52 +147,41 @@ class ApkList(QtWidgets.QListWidget):
         return self.currentItem().text()
 
     def delete_apk(self):
-        msg = QtWidgets.QMessageBox()
-        msg.setIcon(QtWidgets.QMessageBox.Icon.Question)
-
-        msg.setText("Are you sure you want to delete this APK?")
-        msg.setInformativeText("This will delete the APK file.")
-        msg.setWindowTitle("Confirm APK Deletion")
-        msg.setStandardButtons(
-            QtWidgets.QMessageBox.StandardButton.Yes  # type: ignore
-            | QtWidgets.QMessageBox.StandardButton.No
+        ui_dialog.Dialog.yes_no_box(
+            icon=QtWidgets.QMessageBox.Icon.Question,
+            text="Are you sure you want to delete this APK?",
+            informative_text="This will delete the APK file.",
+            window_title="Confirm APK Deletion",
+            default_button=QtWidgets.QMessageBox.StandardButton.No,
+            on_yes=self.delete_apk_call,
         )
-        msg.setDefaultButton(QtWidgets.QMessageBox.StandardButton.No)
-        msg.buttonClicked.connect(self.delete_apk_call)  # type: ignore
 
-        msg.exec_()
-
-    def delete_apk_call(self, button: QtWidgets.QPushButton):
-        if "Yes" in button.text():
-            apk = io.apk.Apk.from_format_string(self.get_selected_apk())
-            apk.delete()
-            self.takeItem(self.currentRow())
+    def delete_apk_call(self):
+        apk = io.apk.Apk.from_format_string(self.get_selected_apk())
+        apk.delete()
+        self.takeItem(self.currentRow())
 
     def extract_apk(self):
         if not io.apk.Apk.check_apktool_installed():
-            msg = QtWidgets.QMessageBox()
-            msg.setIcon(QtWidgets.QMessageBox.Icon.Warning)
-
-            msg.setText("APKTool is not installed.")
-            msg.setInformativeText(
-                "APKTool is required to extract APK files. Install from https://ibotpeaches.github.io/Apktool/install/. Would you like to open the APKTool installation page?"
+            ui_dialog.Dialog.yes_no_box(
+                icon=QtWidgets.QMessageBox.Icon.Warning,
+                text="APKTool is not installed.",
+                informative_text="APKTool is required to extract APK files. Install from https://ibotpeaches.github.io/Apktool/install/. Would you like to open the APKTool installation page?",
+                window_title="APKTool Not Installed",
+                default_button=QtWidgets.QMessageBox.StandardButton.No,
+                on_yes=self.open_apktool_install_page,
             )
-            msg.setWindowTitle("APKTool Not Installed")
-            msg.setStandardButtons(
-                QtWidgets.QMessageBox.StandardButton.Yes  # type: ignore
-                | QtWidgets.QMessageBox.StandardButton.No
-            )
-            msg.setDefaultButton(QtWidgets.QMessageBox.StandardButton.No)
-            msg.buttonClicked.connect(self.open_apktool_install_page)  # type: ignore
-
-            msg.exec_()
         else:
             apk = io.apk.Apk.from_format_string(self.get_selected_apk())
             apk.extract()
+            apk.extracted_path.open()
 
-    def open_apktool_install_page(self, button: QtWidgets.QPushButton):
-        if "Yes" in button.text():
-            webbrowser.open("https://ibotpeaches.github.io/Apktool/install/")
+    def open_apktool_install_page(self):
+        webbrowser.open("https://ibotpeaches.github.io/Apktool/install/")
+
+    def select_apk(self):
+        apk = io.apk.Apk.from_format_string(self.get_selected_apk())
+        self.on_select_apk(apk)
 
 
 class ApkDownloader(QtWidgets.QWidget):
@@ -179,6 +194,7 @@ class ApkDownloader(QtWidgets.QWidget):
         super(ApkDownloader, self).__init__(parent)
         self.add_call = add_call
         self.go_back_call = go_back_call
+        self.downloading_apks: list[str] = []
         self.setup_ui()
 
     def setup_ui(self):
@@ -205,9 +221,23 @@ class ApkDownloader(QtWidgets.QWidget):
         self.back_button.clicked.connect(self.go_back_call)  # type: ignore
 
     def download_apk(self, apk: io.apk.Apk):
-        print("Downloading APK: " + apk.format())
-        apk.download_apk()
-        self.add_call(apk)
+        apk_id = apk.get_id()
+        if apk_id in self.downloading_apks:
+            return
+        title = f"Downloading {apk.format()}"
+        self.downloading_apks.append(apk_id)
+        self.progress_bar = progress.ProgressBar(title, self)
+        self._layout.addWidget(self.progress_bar)
+        self._thread = ui_thread.ThreadWorker.run_in_thread(self.download_thread, apk)
+
+    def download_thread(self, apk: io.apk.Apk):
+        apk_id = apk.get_id()
+        apk.download_apk(self.progress_bar.set_progress_full)
+        if apk_id in self.downloading_apks:
+            self.downloading_apks.remove(apk_id)
+
+        if not self.downloading_apks:
+            self.add_call(apk)
 
 
 class DownloadableApksList(QtWidgets.QWidget):
@@ -234,11 +264,14 @@ class DownloadableApksList(QtWidgets.QWidget):
         self.apk_list = QtWidgets.QListWidget()
         self.list_layout.addWidget(self.apk_list)
 
+        self._thread = ui_thread.ThreadWorker.run_in_thread(self.load_apks)
+
+        self.apk_list.itemDoubleClicked.connect(self.download)  # type: ignore
+
+    def load_apks(self):
         self.apk_versions = io.apk.Apk.get_all_versions(self.cc)
         for apk_version in self.apk_versions:
             self.apk_list.addItem(apk_version.format())
-
-        self.apk_list.itemDoubleClicked.connect(self.download)  # type: ignore
 
     def get_selected_apk(self):
         return self.apk_list.currentItem().text()
