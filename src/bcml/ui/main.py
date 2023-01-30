59a9f71ffb51c869dc1e989023003473d19cc1cf
fieryhenry <github@henrythomas.me.uk>
2023-01-30 19:11:06 +0000

    Added way to save changes when exiting

--- src/bcml/ui/main.py
+++ src/bcml/ui/main.py
@@ -1,7 +1,64 @@
-from typing import Optional
+from typing import Any, Callable, Optional
 from PyQt5 import QtCore, QtGui, QtWidgets
-from bcml.ui import apk_manager
+from bcml.ui import apk_manager, mod_manager, ui_dialog
 from bcml.core import io
+
+
+class Change:
+    def __init__(
+        self,
+        on_has_changes: Callable[[], bool],
+        on_save: Callable[..., Any],
+        on_has_changes_args: Optional[list[Any]] = None,
+        save_args: Optional[list[Any]] = None,
+    ):
+        self.on_has_changes = on_has_changes
+        self.on_save = on_save
+        if save_args is None:
+            save_args = []
+        if on_has_changes_args is None:
+            on_has_changes_args = []
+        self.save_args = save_args
+        self.on_has_changes_args = on_has_changes_args
+
+    def save(self):
+        if self.has_changes():
+            self.on_save(*self.save_args)
+
+    def has_changes(self):
+        return self.on_has_changes(*self.on_has_changes_args)
+
+
+class Changes:
+    def __init__(self):
+        self._changes: list[Change] = []
+        self.canceled = False
+
+    def add(self, change: Change):
+        self._changes.append(change)
+
+    def save(self):
+        if self.has_changes():
+            ui_dialog.Dialog.save_changes_dialog(
+                self.on_save, self.clear, self.on_cancel
+            )
+
+    def on_save(self):
+        for change in self._changes:
+            change.save()
+        self.clear()
+
+    def clear(self):
+        self._changes = []
+
+    def has_changes(self):
+        for change in self._changes:
+            if change.has_changes():
+                return True
+        return False
+
+    def on_cancel(self):
+        self.canceled = True
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -11,8 +68,6 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def create_toolbar(self):
         self.toolbar = QtWidgets.QToolBar()
-        self.toolbar.setMovable(False)
-        self.toolbar.setFloatable(False)
         self.toolbar.setToolButtonStyle(
             QtCore.Qt.ToolButtonStyle.ToolButtonTextBesideIcon
         )
@@ -30,6 +85,20 @@ class MainWindow(QtWidgets.QMainWindow):
         icon_path = io.path.Path(is_relative=True).add("assets", "icon.png")
         self.setWindowIcon(QtGui.QIcon(str(icon_path)))
 
+        self.centralwidget = QtWidgets.QWidget(self)
+        self.centralwidget.setObjectName("centralwidget")
+        self.setCentralWidget(self.centralwidget)
+
+        self._layout = QtWidgets.QVBoxLayout()
+        self.centralwidget.setLayout(self._layout)
+
+        self.changes = Changes()
+
+        self.mod_view = mod_manager.ModView(self.changes, self)
+        self._layout.addWidget(self.mod_view)
+
+        # self.check_apk_selected()
+
     def run(self):
         self.show()
         self.raise_()
@@ -43,11 +112,29 @@ class MainWindow(QtWidgets.QMainWindow):
             event.key() == QtCore.Qt.Key.Key_C
             and event.modifiers() == QtCore.Qt.KeyboardModifier.ControlModifier
         ):
-            self.close()
+            self.close_wrapper()
 
     def open_apk_manager(self):
         self.apk_man = apk_manager.ApkManager()
         self.apk_man.show()
+
+    def check_apk_selected(self):
+        if not io.config.Config().get(io.config.Key.SELECTED_APK):
+            self.open_apk_manager()
+
+    def close_wrapper(self) -> bool:
+        self.changes.save()
+        if self.changes.canceled:
+            self.changes.canceled = False
+            return False
+        return True
+
+    def closeEvent(self, event: QtGui.QCloseEvent):  # type: ignore
+        close = self.close_wrapper()
+        if close:
+            event.accept()
+        else:
+            event.ignore()
 
 
 def clear_layout(layout: QtWidgets.QLayout):
--- src/bcml/ui/ui_dialog.py
+++ src/bcml/ui/ui_dialog.py
@@ -7,11 +7,17 @@ class Dialog:
         pass
 
     @staticmethod
-    def error_dialog(message: str):
+    def error_dialog(
+        message: str,
+        informative_text: str = "",
+        title: str = "Error",
+    ):
         dialog = QtWidgets.QMessageBox()
         dialog.setIcon(QtWidgets.QMessageBox.Icon.Critical)
         dialog.setText(message)
-        dialog.setWindowTitle("Error")
+        dialog.setInformativeText(informative_text)
+        dialog.setWindowTitle(title)
+
         dialog.exec_()
 
     @staticmethod
@@ -38,3 +44,55 @@ class Dialog:
             lambda button: on_yes() if "Yes" in button.text() else on_no()  # type: ignore
         )
         msg.exec_()
+
+    @staticmethod
+    def three_button_box(
+        icon: QtWidgets.QMessageBox.Icon,
+        text: str,
+        informative_text: str,
+        window_title: str,
+        default_button: QtWidgets.QMessageBox.StandardButton,
+        on_yes: Callable[..., Any] = lambda: None,
+        on_no: Callable[..., Any] = lambda: None,
+        on_cancel: Callable[..., Any] = lambda: None,
+    ):
+        msg = QtWidgets.QMessageBox()
+        msg.setIcon(icon)
+        msg.setText(text)
+        msg.setInformativeText(informative_text)
+        msg.setWindowTitle(window_title)
+        msg.setStandardButtons(
+            QtWidgets.QMessageBox.StandardButton.Yes  # type: ignore
+            | QtWidgets.QMessageBox.StandardButton.No
+            | QtWidgets.QMessageBox.StandardButton.Cancel
+        )
+        msg.setDefaultButton(default_button)
+        msg.buttonClicked.connect(  # type: ignore
+            lambda button: on_yes() if "Yes" in button.text() else on_no() if "No" in button.text() else on_cancel()  # type: ignore
+        )
+        msg.exec_()
+
+    @staticmethod
+    def info_dialog(message: str):
+        dialog = QtWidgets.QMessageBox()
+        dialog.setIcon(QtWidgets.QMessageBox.Icon.Information)
+        dialog.setText(message)
+        dialog.setWindowTitle("Info")
+        dialog.exec_()
+
+    @staticmethod
+    def save_changes_dialog(
+        on_yes: Callable[..., Any] = lambda: None,
+        on_no: Callable[..., Any] = lambda: None,
+        on_cancel: Callable[..., Any] = lambda: None,
+    ):
+        Dialog.three_button_box(
+            QtWidgets.QMessageBox.Icon.Warning,
+            "Save changes?",
+            "You have unsaved changes. Do you want to save them?",
+            "Save changes?",
+            QtWidgets.QMessageBox.StandardButton.Yes,
+            on_yes,
+            on_no,
+            on_cancel,
+        )
