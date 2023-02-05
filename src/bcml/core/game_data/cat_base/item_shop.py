3350b7106eb3b845dc0164e76c8a942bad4f770f
fieryhenry <github@henrythomas.me.uk>
2023-02-05 15:17:58 +0000

    Added docs + fixed imgcut generation

--- src/bcml/core/game_data/cat_base/item_shop.py
+++ src/bcml/core/game_data/cat_base/item_shop.py
@@ -4,6 +4,8 @@ from bcml.core import io
 
 
 class Item:
+    """Represents an item in the Item Shop."""
+
     def __init__(
         self,
         shop_id: int,
@@ -15,6 +17,18 @@ class Item:
         imgcut_id: int,
         cut: "bc_anim.Cut",
     ):
+        """Initialize a new Item.
+
+        Args:
+            shop_id (int): The index of the item in the shop.
+            gatya_item_id (int): The ID of the item to sell.
+            count (int): The number of items to sell.
+            price (int): The price of the item in catfood.
+            draw_item_value (bool): Whether to draw the player's current item count of the item.
+            category_name (str): The name of the category the item belongs to. e.g "Battle Items", "XP"
+            imgcut_id (int): The index in the imgcut of the item's icon.
+            cut (bc_anim.Cut): The image of the item in the shop.
+        """
         self.shop_id = shop_id
         self.gatya_item_id = gatya_item_id
         self.count = count
@@ -25,6 +39,11 @@ class Item:
         self.cut = cut
 
     def serialize(self) -> dict[str, Any]:
+        """Serialize the Item to a dict.
+
+        Returns:
+            dict[str, Any]: The serialized Item.
+        """
         return {
             "shop_id": self.shop_id,
             "gatya_item_id": self.gatya_item_id,
@@ -33,12 +52,20 @@ class Item:
             "draw_item_value": self.draw_item_value,
             "category_name": self.category_name,
             "imgcut_id": self.imgcut_id,
-            "cut": self.cut.serialize(),
+            "cut": self.cut.serialize(img=True),
         }
 
     @staticmethod
     def deserialize(data: dict[str, Any]) -> "Item":
-        return Item(
+        """Deserialize an Item from a dict.
+
+        Args:
+            data (dict[str, Any]): The serialized Item.
+
+        Returns:
+            Item: The deserialized Item.
+        """
+        item = Item(
             data["shop_id"],
             data["gatya_item_id"],
             data["count"],
@@ -48,33 +75,85 @@ class Item:
             data["imgcut_id"],
             bc_anim.Cut.deserialize(data["cut"]),
         )
+        return item
+
+    def __str__(self) -> str:
+        """Get a string representation of the Item.
+
+        Returns:
+            str: The string representation of the Item.
+        """
+        return f"Item({self.shop_id}, {self.gatya_item_id}, {self.count}, {self.price}, {self.draw_item_value}, {self.category_name}, {self.imgcut_id}, {self.cut})"
+
+    def __repr__(self) -> str:
+        """Get a string representation of the Item.
+
+        Returns:
+            str: The string representation of the Item.
+        """
+        return str(self)
 
 
 class ItemShop:
-    def __init__(self, items: dict[int, Item], imgcut: "bc_anim.Imgcut"):
+    """Represents the Item Shop."""
+
+    def __init__(
+        self,
+        items: dict[int, Item],
+        imgname: str,
+    ):
+        """Initialize a new ItemShop.
+
+        Args:
+            items (dict[int, Item]): The items in the shop.
+            imgname (str): The name of the image file containing the shop's icons.
+        """
         self.items = items
-        self.imgcut = imgcut
+        self.imgname = imgname
 
     def serialize(self) -> dict[str, Any]:
+        """Serialize the ItemShop to a dict.
+
+        Returns:
+            dict[str, Any]: The serialized ItemShop.
+        """
         return {
             "items": {str(k): v.serialize() for k, v in self.items.items()},
-            "imgcut": self.imgcut.serialize(),
+            "imgname": self.imgname,
         }
 
     @staticmethod
     def deserialize(data: dict[str, Any]) -> "ItemShop":
-        imgcut = bc_anim.Imgcut.deserialize(data["imgcut"])
-        return ItemShop(
-            {int(k): Item.deserialize(v) for k, v in data["items"].items()},
-            imgcut,
-        )
+        """Deserialize an ItemShop from a dict.
+
+        Args:
+            data (dict[str, Any]): The serialized ItemShop.
+
+        Returns:
+            ItemShop: The deserialized ItemShop.
+        """
+        items = {int(k): Item.deserialize(v) for k, v in data["items"].items()}
+        return ItemShop(items, data["imgname"])
 
     @staticmethod
     def get_file_name() -> str:
+        """Get the name of the file containing the ItemShop data.
+
+        Returns:
+            str: The name of the file containing the ItemShop data.
+        """
         return "itemShopData.tsv"
 
     @staticmethod
-    def from_game_data(game_data: "pack.GamePacks") -> Optional["ItemShop"]:
+    def from_game_data(game_data: "pack.GamePacks") -> "ItemShop":
+        """Get the ItemShop from the game data.
+
+        Args:
+            game_data (pack.GamePacks): The game data.
+
+        Returns:
+            ItemShop: The ItemShop.
+        """
         tsv_data = game_data.find_file(ItemShop.get_file_name())
         png_data = game_data.find_file(
             f"item000_{game_data.country_code.get_language()}.png"
@@ -83,12 +162,14 @@ class ItemShop:
             f"item000_{game_data.country_code.get_language()}.imgcut"
         )
         if tsv_data is None or png_data is None or imgcut_data is None:
-            return None
+            return ItemShop.create_empty()
         img = io.bc_image.BCImage(png_data.dec_data)
         imgcut = bc_anim.Imgcut.from_data(imgcut_data.dec_data, img)
         tsv = io.bc_csv.CSV(tsv_data.dec_data, delimeter="\t")
         items = {}
         for line in tsv.lines[1:]:
+            cut = imgcut.cuts[line[6].to_int()]
+            cut.get_image(img)
             items[line[0].to_int()] = Item(
                 line[0].to_int(),
                 line[1].to_int(),
@@ -97,14 +178,30 @@ class ItemShop:
                 line[4].to_bool(),
                 line[5].to_str(),
                 line[6].to_int(),
-                imgcut.cuts[line[6].to_int()],
+                cut,
             )
-        return ItemShop(items, imgcut)
+        return ItemShop(items, imgcut.image_name)
+
+    def get_imgcut(self) -> "bc_anim.Imgcut":
+        """Get the Imgcut of the ItemShop.
+
+        Returns:
+            bc_anim.Imgcut: The Imgcut of the ItemShop.
+        """
+        cuts: list[bc_anim.Cut] = []
+        for item in self.items.values():
+            cuts.append(item.cut)
+        return bc_anim.Imgcut.from_cuts(cuts, self.imgname)
 
     def to_game_data(self, game_data: "pack.GamePacks"):
+        """Write the ItemShop to the game data.
+
+        Args:
+            game_data (pack.GamePacks): The game data.
+        """
         tsv_data = game_data.find_file(ItemShop.get_file_name())
         if tsv_data is None:
-            return None
+            return
         tsv = io.bc_csv.CSV(tsv_data.dec_data, delimeter="\t")
         remaning_items = self.items.copy()
         for i, line in enumerate(tsv.lines[1:]):
@@ -133,9 +230,10 @@ class ItemShop:
             tsv.add_line(line)
 
         game_data.set_file(ItemShop.get_file_name(), tsv.to_data())
-        if not self.imgcut.is_empty():
-            self.imgcut.reconstruct_image()
-            csv_data, png_data = self.imgcut.to_data()
+        imgcut = self.get_imgcut()
+        if not imgcut.is_empty():
+            imgcut.reconstruct_image()
+            csv_data, png_data = imgcut.to_data()
             game_data.set_file(
                 f"item000_{game_data.country_code.get_language()}.png", png_data
             )
@@ -145,14 +243,32 @@ class ItemShop:
 
     @staticmethod
     def get_json_file_path() -> "io.path.Path":
+        """Get the path of the json file containing the ItemShop data.
+
+        Returns:
+            io.path.Path: The path of the json file containing the ItemShop data.
+        """
         return io.path.Path("catbase").add("item_shop.json")
 
     def add_to_zip(self, zip_file: "io.zip.Zip"):
+        """Add the ItemShop to a zip file.
+
+        Args:
+            zip_file (io.zip.Zip): The zip file.
+        """
         json = io.json_file.JsonFile.from_json(self.serialize())
         zip_file.add_file(ItemShop.get_json_file_path(), json.to_data())
 
     @staticmethod
     def from_zip(zip: "io.zip.Zip") -> "ItemShop":
+        """Create an ItemShop from a zip file.
+
+        Args:
+            zip (io.zip.Zip): The zip file.
+
+        Returns:
+            ItemShop: The ItemShop.
+        """
         json_data = zip.get_file(ItemShop.get_json_file_path())
         if json_data is None:
             return ItemShop.create_empty()
@@ -161,16 +277,78 @@ class ItemShop:
 
     @staticmethod
     def create_empty() -> "ItemShop":
-        return ItemShop({}, bc_anim.Imgcut.create_empty())
+        """Create an empty ItemShop.
+
+        Returns:
+            ItemShop: The empty ItemShop.
+        """
+        return ItemShop({}, "")
 
     def get_item(self, shop_index: int) -> Optional[Item]:
+        """Get an item from the ItemShop.
+
+        Args:
+            shop_index (int): The index of the item in the ItemShop.
+
+        Returns:
+            Optional[Item]: The item.
+        """
         return self.items.get(shop_index)
 
     def set_item(self, shop_index: int, item: Item):
+        """Set an item in the ItemShop.
+
+        Args:
+            shop_index (int): The index of the item in the ItemShop.
+            item (Item): The item.
+        """
         item.shop_id = shop_index
         self.items[shop_index] = item
-        self.imgcut.set_cut(item.imgcut_id, item.cut)
 
     def import_item_shop(self, other: "ItemShop"):
+        """Import an ItemShop into this ItemShop.
+
+        Args:
+            other (ItemShop): The ItemShop to import.
+        """
         for item in other.items.values():
             self.set_item(item.shop_id, item)
+
+    def add_item(self, item: Item):
+        """Add an item to the ItemShop.
+
+        Args:
+            item (Item): The item to add.
+        """
+        self.set_item(item.shop_id, item)
+
+    def remove_item(self, shop_index: int):
+        """Remove an item from the ItemShop.
+
+        Args:
+            shop_index (int): The index of the item in the ItemShop.
+        """
+        self.items.pop(shop_index)
+        self.shift_items(shop_index + 1, -1)
+
+    def shift_items(self, start_index: int, shift: int):
+        """Move all items after a certain index.
+
+        Args:
+            start_index (int): The index to start shifting from.
+            shift (int): The amount to shift by.
+        """
+        for item in self.items.values():
+            if item.shop_id >= start_index:
+                item.shop_id += shift
+        self.items = {item.shop_id: item for item in self.items.values()}
+
+    def insert_item(self, shop_index: int, item: Item):
+        """Insert an item into the ItemShop.
+
+        Args:
+            shop_index (int): The index to insert the item at.
+            item (Item): The item to insert.
+        """
+        self.shift_items(shop_index, 1)
+        self.set_item(shop_index, item)
