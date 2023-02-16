47dd105ec14ee90678d894a25ea15db89844004b
fieryhenry <github@henrythomas.me.uk>
2023-02-16 10:20:09 +0000

    Added localizable

--- src/bcml/ui/__init__.py
+++ src/bcml/ui/__init__.py
@@ -11,6 +11,7 @@ from bcml.ui import (
     game_editor,
     shop_editor,
     gatya_item,
+    localizable,
 )
 
 __all__ = [
@@ -26,4 +27,5 @@ __all__ = [
     "game_editor",
     "shop_editor",
     "gatya_item",
+    "localizable",
 ]
