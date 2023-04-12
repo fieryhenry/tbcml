1b87b46a45b01d96258cf3447a96460f39526823
fieryhenry <github@henrythomas.me.uk>
2023-04-12 16:39:46 +0100

    added docs

--- src/bcml/core/langs.py
+++ src/bcml/core/langs.py
@@ -2,6 +2,8 @@ import enum
 
 
 class Languages(enum.Enum):
+    """Enum for all supported languages in the en version of the game."""
+
     DE = "de"
     FR = "fr"
     IT = "it"
@@ -10,8 +12,18 @@ class Languages(enum.Enum):
 
     @staticmethod
     def get_all() -> list["Languages"]:
+        """Gets all languages.
+
+        Returns:
+            list[Languages]: All languages.
+        """
         return list(Languages)
 
     @staticmethod
     def get_all_strings() -> list[str]:
+        """Gets all languages as strings.
+
+        Returns:
+            list[str]: All languages as strings.
+        """
         return [lang.value for lang in Languages.get_all()]
