b7465cbc46922facdaa6a0f47831b02bd196752f
fieryhenry <github@henrythomas.me.uk>
2023-04-12 16:30:08 +0100

    added docs

--- src/bcml/core/server_handler.py
+++ src/bcml/core/server_handler.py
@@ -12,12 +12,30 @@ from cryptography.hazmat.primitives.asymmetric import padding
 
 
 class ServerFileHandler:
+    """Class for handling downloading game files from the game server"""
+
     def __init__(self, apk: "io.apk.Apk"):
+        """Initializes the ServerFileHandler class
+
+        Args:
+            apk (io.apk.Apk): The APK object to use, used for country code and game version list
+        """
         self.apk = apk
         self.tsv_paths = self.apk.get_download_tsvs()
         self.game_versions = self.find_game_versions()
 
     def parse_tsv(self, index: int) -> "io.bc_csv.CSV":
+        """Parses a TSV file from the APK
+
+        Args:
+            index (int): The index of the TSV file to parse
+
+        Raises:
+            ValueError: If the index is invalid
+
+        Returns:
+            io.bc_csv.CSV: The parsed TSV file
+        """
         if index >= len(self.tsv_paths) or index < 0:
             raise ValueError("Invalid TSV index")
         tsv_path = self.tsv_paths[index]
@@ -25,11 +43,30 @@ class ServerFileHandler:
         return tsv
 
     def get_game_version(self, index: int) -> int:
+        """Gets the game version from the libnative.so file
+
+        Args:
+            index (int): The index of the game version to get, aligns with the TSV file
+
+        Raises:
+            ValueError: If the index is invalid
+
+        Returns:
+            int: The game version
+        """
         if index >= len(self.game_versions) or index < 0:
-            raise ValueError("Invalid TSV index")
+            raise ValueError("Invalid index")
         return self.game_versions[index]
 
-    def get_url(self, index: int):
+    def get_url(self, index: int) -> str:
+        """Gets the download URL for a game version
+
+        Args:
+            index (int): The index of the game version to get
+
+        Returns:
+            str: The download URL
+        """
         game_version = self.get_game_version(index)
         project_name = f"battlecats{self.apk.country_code.get_patching_code()}"
         str_code = ""
@@ -51,6 +88,18 @@ class ServerFileHandler:
         index: int,
         progress_callback: Optional[Callable[[float, int, int], None]] = None,
     ) -> "io.zip.Zip":
+        """Downloads game files from the server for a given game version
+
+        Args:
+            index (int): The index of the game version to download
+            progress_callback (Optional[Callable[[float, int, int], None]], optional): A callback function to call with progress updates. Defaults to None.
+
+        Raises:
+            ValueError: If the zip data is invalid
+
+        Returns:
+            io.zip.Zip: The downloaded game files
+        """
         url = self.get_url(index)
         cloudfront = CloudFront()
         signed_cookie = cloudfront.generate_signed_cookie(
@@ -81,6 +130,16 @@ class ServerFileHandler:
         progress_callback: Optional[Callable[[float, int, int], None]] = None,
         force: bool = False,
     ) -> bool:
+        """Extracts game files to the server files path for a given game version
+
+        Args:
+            index (int): The index of the game version to extract
+            progress_callback (Optional[Callable[[float, int, int], None]], optional): A callback function to call with progress updates. Defaults to None.
+            force (bool, optional): Whether to force download even if the files already exist. Defaults to False.
+
+        Returns:
+            bool: Whether the files were extracted
+        """
         tsv = self.parse_tsv(index)
         if not force:
             found = True
@@ -116,6 +175,13 @@ class ServerFileHandler:
         progress_callback: Optional[Callable[[int, int], None]] = None,
         force: bool = False,
     ):
+        """Extracts all game versions
+
+        Args:
+            progress_callback_individual (Optional[ Callable[[float, int, int], None] ], optional): Callback function to call with progress updates for each game version. Defaults to None.
+            progress_callback (Optional[Callable[[int, int], None]], optional): Callback function to call with progress updates for each game version. Defaults to None.
+            force (bool, optional): Whether to force extraction even if the files already exist. Defaults to False.
+        """
         if progress_callback:
             progress_callback(0, len(self.tsv_paths))
         for i in range(len(self.tsv_paths)):
@@ -125,7 +191,18 @@ class ServerFileHandler:
             if progress_callback is not None:
                 progress_callback(i + 1, len(self.tsv_paths))
 
-    def find_game_versions(self):
+    def find_game_versions(self) -> list[int]:
+        """Finds all game versions in the libnative.so file
+
+        Raises:
+            ValueError: If the libnative.so file could not be found
+            ValueError: If the architecture could not be found
+            ValueError: If the country code is not supported
+            ValueError: If no game versions were found
+
+        Returns:
+            list[int]: A list of game versions
+        """
         arcs = self.apk.get_architectures()
         lib = None
         arc = None
@@ -161,7 +238,10 @@ class ServerFileHandler:
 
 
 class CloudFront:
+    """A class for interacting with the game's CloudFront aws server for downloading game files"""
+
     def __init__(self):
+        """Initializes the CloudFront class"""
         self.cf_key_pair_id = "APKAJO6MLYTURWB2NOWQ"
         self.cf_private_key = """
 -----BEGIN PRIVATE KEY-----
@@ -193,7 +273,15 @@ Tx8VMiCUkL+5uNJwvWw2rrEHvt2jphD016pgdutlgI28qoXwcleLxAz1Ey1njCTO
 oPSxLzYw2sBjmwVooXMVr6GxEw==
 -----END PRIVATE KEY-----"""
 
-    def make_policy(self, url: str):
+    def make_policy(self, url: str) -> str:
+        """Makes a policy for the given url
+
+        Args:
+            url (str): URL to make policy for
+
+        Returns:
+            str: Policy
+        """
         policy = {
             "Statement": [
                 {
@@ -211,32 +299,81 @@ oPSxLzYw2sBjmwVooXMVr6GxEw==
         }
         return json.dumps(policy).replace(" ", "")
 
-    def make_signature(self, message: str):
+    def make_signature(self, message: str) -> bytes:
+        """Makes a signature for the given message
+
+        Args:
+            message (str): Message to make signature for
+
+        Returns:
+            bytes: Signature
+        """
         private_key = serialization.load_pem_private_key(
             self.cf_private_key.encode(), password=None, backend=default_backend()
         )
         return private_key.sign(message.encode(), padding.PKCS1v15(), hashes.SHA1())
 
     @staticmethod
-    def aws_base64_encode(data: bytes):
+    def aws_base64_encode(data: bytes) -> bytes:
+        """Encodes data in base64
+
+        Args:
+            data (bytes): Data to encode
+
+        Returns:
+            bytes: Encoded data
+        """
         return base64.b64encode(data)
 
     @staticmethod
-    def aws_base64_decode(data: bytes):
+    def aws_base64_decode(data: bytes) -> bytes:
+        """Decodes data from base64
+
+        Args:
+            data (bytes): Data to decode
+
+        Returns:
+            bytes: Decoded data
+        """
         return base64.b64decode(data)
 
-    def generate_signature(self, policy: str):
+    def generate_signature(self, policy: str) -> str:
+        """Generates a signature for the given policy
+
+        Args:
+            policy (str): Policy to generate signature for
+
+        Returns:
+            str: Signature
+        """
         signature = self.make_signature(policy)
         return self.aws_base64_encode(signature).decode()
 
-    def generate_signed_cookie(self, url: str):
+    def generate_signed_cookie(self, url: str) -> str:
+        """Generates a signed cookie for the given url
+
+        Args:
+            url (str): URL to generate signed cookie for
+
+        Returns:
+            str: Signed cookie
+        """
         policy = self.make_policy(url)
         signature = self.generate_signature(policy)
         return f"CloudFront-Key-Pair-Id={self.cf_key_pair_id}; CloudFront-Policy={self.aws_base64_encode(policy.encode()).decode()}; CloudFront-Signature={signature}"
 
 
 class EventData:
+    """Class for downloading event data from the game server
+    Lots of this code is taken from the PackPack discord bot: https://github.com/battlecatsultimate/PackPack
+    """
+
     def __init__(self, file_name: str):
+        """Initializes the class
+
+        Args:
+            file_name (str): File to download
+        """
         self.aws_access_key_id = "AKIAJCUP3WWCHRJDTPPQ"
         self.aws_secret_access_key = "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"
         self.region = "ap-northeast-1"
@@ -247,7 +384,12 @@ class EventData:
 
         self.url = f"https://{self.domain}/battlecatsen_production/{file_name}"
 
-    def get_auth_header(self):
+    def get_auth_header(self) -> str:
+        """Gets the authorization header
+
+        Returns:
+            str: Authorization header
+        """
         output = self.algorithm + " "
         output += f"Credential={self.aws_access_key_id}/{self.get_date()}/{self.region}/{self.service}/{self.request}, "
         output += f"SignedHeaders=host;x-amz-content-sha256;x-amz-date, "
@@ -256,13 +398,31 @@ class EventData:
 
         return output
 
-    def get_date(self):
+    def get_date(self) -> str:
+        """Gets the date
+
+        Returns:
+            str: Date in YYYYMMDD format
+        """
         return datetime.datetime.utcnow().strftime("%Y%m%d")
 
-    def get_amz_date(self):
+    def get_amz_date(self) -> str:
+        """Gets the amz date
+
+        Returns:
+            str: Date in YYYYMMDDTHHMMSSZ format
+        """
         return datetime.datetime.utcnow().strftime("%Y%m%dT%H%M%SZ")
 
-    def get_signing_key(self, amz: str):
+    def get_signing_key(self, amz: str) -> "io.data.Data":
+        """Gets the signing key for the given amz date
+
+        Args:
+            amz (str): Amz date
+
+        Returns:
+            io.data.Data: Signing key
+        """
         k = io.data.Data("AWS4" + self.aws_secret_access_key)
         k_date = self.hmacsha256(k, self.get_date())
         date_region_key = self.hmacsha256(k_date, self.region)
@@ -275,11 +435,28 @@ class EventData:
         return final
 
     def hmacsha256(self, key: "io.data.Data", message: str) -> "io.data.Data":
+        """Gets the hmacsha256 of the given key and message
+
+        Args:
+            key (io.data.Data): Key
+            message (str): Message
+
+        Returns:
+            io.data.Data: Hmacsha256 of the given key and message
+        """
         return crypto.Hmac(key, crypto.HashAlgorithm.SHA256).get_hmac(
             io.data.Data(message)
         )
 
-    def get_string_to_sign(self, amz: str):
+    def get_string_to_sign(self, amz: str) -> str:
+        """Gets the string to sign for the given amz date
+
+        Args:
+            amz (str): Amz date
+
+        Returns:
+            str: String to sign
+        """
         output = self.algorithm + "\n"
         output += amz + "\n"
         output += (
@@ -300,7 +477,15 @@ class EventData:
         )
         return output
 
-    def get_canonical_request(self, amz: str):
+    def get_canonical_request(self, amz: str) -> str:
+        """Gets the canonical request for the given amz date
+
+        Args:
+            amz (str): Amz date
+
+        Returns:
+            str: Canonical request
+        """
         output = "GET\n"
         output += self.get_canonical_uri() + "\n" + "\n"
         output += "host:" + self.domain + "\n"
@@ -311,10 +496,20 @@ class EventData:
         output += "UNSIGNED-PAYLOAD"
         return output
 
-    def get_canonical_uri(self):
+    def get_canonical_uri(self) -> str:
+        """Gets the canonical uri for the current url
+
+        Returns:
+            str: Canonical uri, e.g. /battlecatsen_production/...
+        """
         return self.url.split(self.domain)[1]
 
-    def make_request(self):
+    def make_request(self) -> "request.requests.Response":
+        """Makes the request to download the event data
+
+        Returns:
+            request.requests.Response: Response
+        """
         url = self.url
         headers = {
             "accept-encoding": "gzip",
