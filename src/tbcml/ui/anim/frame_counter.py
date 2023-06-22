d230583a80227766ebcde2c7c6a17fef4ac9f896
fieryhenry <github@henrythomas.me.uk>
2023-06-22 14:56:52 +0100

    perm frame

--- src/tbcml/ui/anim/frame_counter.py
+++ src/tbcml/ui/anim/frame_counter.py
@@ -7,20 +7,45 @@ class FrameClock:
         self.fps = fps
         self.timer = QTimer()
         self.timer.timeout.connect(self.tick)
-        self.timer.start(1000 // fps)
         self.frame = 0
+        self.perm_frame = 0
         self.funcs: list[Callable[..., Any]] = []
+        self.perm_funcs: list[Callable[..., Any]] = []
+        self.perm_timer = QTimer()
+        self.perm_timer.timeout.connect(self.tick_perm)
+        self.perm_timer.start(1000 // fps)
+        self.timer.start(1000 // fps)
 
     def tick(self):
         self.frame += 1
         for func in self.funcs:
             func()
 
+    def tick_perm(self):
+        self.perm_frame += 1
+        for func in self.perm_funcs:
+            func()
+
     def add_func(self, func: Callable[..., Any]):
         self.funcs.append(func)
+
+    def add_perm_func(self, func: Callable[..., Any]):
+        self.perm_funcs.append(func)
 
     def remove_func(self, func: Callable[..., Any]):
         self.funcs.remove(func)
 
+    def remove_perm_func(self, func: Callable[..., Any]):
+        self.perm_funcs.remove(func)
+
     def get_frame(self) -> int:
         return self.frame
+
+    def get_perm_frame(self) -> int:
+        return self.perm_frame
+
+    def start(self):
+        self.timer.start(1000 // self.fps)
+
+    def stop(self):
+        self.timer.stop()
