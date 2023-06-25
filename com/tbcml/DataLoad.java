1a06b7c5b2eab74f196eafa165d09375a402869a
fieryhenry <github@henrythomas.me.uk>
2023-06-25 12:41:54 +0100

    url downloading

--- com/tbcml/DataLoad.java
+++ com/tbcml/DataLoad.java
@@ -4,10 +4,16 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.net.URL;
+import java.util.Base64;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
-/* loaded from: /tmp/jadx-11619970748472039267.dex */
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+import android.os.StrictMode;
+
 public class DataLoad {
     public static void Start(Context context) {
         Extract(context);
@@ -34,6 +40,16 @@ public class DataLoad {
                     boolean alwaysCopy = entryName.endsWith(".alwayscopy");
                     entryName = entryName.replace(".alwayscopy", "");
 
+                    Boolean isUrl = entryName.contains("$url:");
+                    // file_name$url:base64url
+                    String url = "";
+                    if (isUrl) {
+                        String burl = entryName.split("\\$url:")[1];
+                        url = new String(Base64.getUrlDecoder().decode(burl));
+
+                        entryName = entryName.split("\\$url:")[0];
+                    }
+
                     File gameFile = new File(gameDataPath + entryName);
                     if (gameFile.exists() && !alwaysCopy) {
                         continue;
@@ -43,7 +59,10 @@ public class DataLoad {
                         if (!gameFile.exists()) {
                             gameFile.mkdirs();
                         }
-                    } else {
+                        continue;
+                    }
+
+                    if (!isUrl) {
                         byte[] data = new byte[2048];
                         FileOutputStream fos = new FileOutputStream(gameFile);
                         BufferedOutputStream dest2 = new BufferedOutputStream(fos, 2048);
@@ -56,7 +75,28 @@ public class DataLoad {
                         }
                         dest2.flush();
                         dest2.close();
+                        fos.close();
+                        continue;
                     }
+                    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                    StrictMode.setThreadPolicy(policy);
+                    Log.d("tbcml", "Downloading " + url + " to " + gameFile.getAbsolutePath());
+                    URL url2 = new URL(url);
+                    InputStream localInputStream2 = url2.openStream();
+                    byte[] data2 = new byte[2048];
+                    FileOutputStream fos2 = new FileOutputStream(gameFile);
+                    BufferedOutputStream dest22 = new BufferedOutputStream(fos2, 2048);
+                    while (true) {
+                        int count2 = localInputStream2.read(data2, 0, 2048);
+                        if (count2 == -1) {
+                            break;
+                        }
+                        dest22.write(data2, 0, count2);
+                    }
+                    dest22.flush();
+                    dest22.close();
+                    fos2.close();
+
                 } catch (Exception e2) {
                     e = e2;
                     e.printStackTrace();
