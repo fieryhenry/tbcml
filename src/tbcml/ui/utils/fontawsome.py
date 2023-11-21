3ff7ea7e18748477599d8a66ae9bd4790fd306f5
fieryhenry <github@henrythomas.me.uk>
2023-11-21 20:20:37 +0000

    don't crash if not installed

--- src/tbcml/ui/utils/fontawsome.py
+++ src/tbcml/ui/utils/fontawsome.py
@@ -1,6 +1,13 @@
-import qtawesome as qta
+try:
+    import qtawesome as qta
+except ImportError:
+    qta = None
 from PyQt5.QtGui import QIcon
 
 
 def get_icon(icon_name: str) -> QIcon:
+    if qta is None:
+        print("Please pip install tbcml[ui] to use this feature")
+        return QIcon()
+
     return qta.icon(f"fa5s.{icon_name}", color="white")  # type: ignore
