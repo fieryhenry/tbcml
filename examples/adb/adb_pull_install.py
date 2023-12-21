821583f32a374b95e75bd68f2cb368892ac29e2a
fieryhenry <github@henrythomas.me.uk>
2023-12-21 20:31:35 +0000

    adb pull and install example

---
+++ examples/adb/adb_pull_install.py
@@ -0,0 +1,30 @@
+from tbcml import core
+
+adb_handler = core.AdbHandler("jp.co.ponos.battlecatsen")
+
+devices = adb_handler.get_connected_devices()
+print(devices)
+if len(devices) == 0:
+    raise ValueError("No devices connected")
+adb_handler.set_device(devices[0])
+
+apk = adb_handler.pull_apk()
+# can overwrite autodetected cc and gv with adb_handler.pull_apk(cc, gv)
+
+if apk is None:
+    raise ValueError("Failed to pull apk")
+
+apk.extract()
+apk.download_server_files(display=True)
+
+apk.set_app_name("ADB New")
+apk.set_package_name("jp.co.ponos.battlecatsen.adb")
+
+apk.load_mods([])
+
+print(apk.get_final_apk_path())
+
+adb_handler.install_apk(apk.get_final_apk_path())
+adb_handler.run_game()
+
+# cProfile.run("do()", sort="cumtime")
