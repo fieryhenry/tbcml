ca81f05e38b426eaf047b0d803231d95d521f190
fieryhenry <github@henrythomas.me.uk>
2023-12-30 15:15:08 +0000

    cc can be a str + can load other mods

--- src/tbcml/core/mods/loader.py
+++ src/tbcml/core/mods/loader.py
@@ -1,5 +1,5 @@
 from tbcml import core
-from typing import List, Union
+from typing import List, Optional, Union
 
 
 class ModLoader:
@@ -7,7 +7,7 @@ class ModLoader:
     The ModLoader class is responsible for loading and managing mods in the game.
 
     Args:
-        country_code (CountryCode): The country code for the game.
+        country_code (str | CountryCode): The country code for the game.
         game_version (str | GameVersion): The version of the game.
         mod_instance (Mod): An instance of the Mod class.
 
@@ -30,21 +30,27 @@ class ModLoader:
 
     def __init__(
         self,
-        country_code: "core.CountryCode",
+        country_code: Union[str, "core.CountryCode"],
         game_version: Union[str, "core.GameVersion"],
         mod_instance: "core.Mod",
     ):
-        self.country_code = country_code
+        if isinstance(country_code, str):
+            self.country_code = core.CountryCode.from_code(country_code)
+        else:
+            self.country_code = country_code
+
         if isinstance(game_version, str):
-            self.game_version = core.GameVersion.from_string(game_version)
+            self.game_version = core.GameVersion.from_string_latest(
+                game_version, self.country_code
+            )
         else:
             self.game_version = game_version
+
         self.mod = mod_instance
 
         # not initialized in constructor
         self.game_packs = None
         self.apk = None
-        self.mods = List[core.Mod]
 
     def initialize(self):
         self.__get_apk()
@@ -62,6 +68,11 @@ class ModLoader:
             pass
 
         self.game_packs = core.GamePacks.from_apk(self.apk)
+
+    def get_game_packs(self) -> "core.GamePacks":
+        if self.game_packs is None:
+            raise Exception("Game packs not initialized. Call initialize() first.")
+        return self.game_packs
 
     def add_cat(self, cat: "core.Cat"):
         edit = core.ModEdit(["cats", cat.cat_id], cat.to_dict())
@@ -81,11 +92,18 @@ class ModLoader:
     def __add_mod_edit(self, edit: "core.ModEdit"):
         self.mod.add_mod_edit(edit)
 
-    def compile(self, open_path: bool):
+    def load(
+        self, open_path: bool = False, other_mods: Optional[List["core.Mod"]] = None
+    ):
         if self.apk is None:
             raise Exception("APK not initialized. Call initialize() first.")
 
-        self.apk.load_mods([self.mod], self.game_packs)
+        if other_mods is not None:
+            mods = [self.mod] + other_mods
+        else:
+            mods = [self.mod]
+
+        self.apk.load_mods(mods, self.game_packs)
 
         if open_path:
             self.apk.output_path.open()
