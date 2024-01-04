455c73484edc9efcb1f26dc714a89fa561e8ae41
fieryhenry <github@henrythomas.me.uk>
2024-01-04 19:26:05 +0000

    updated for new structure

--- examples/cats/name_desc_edit.py
+++ examples/cats/name_desc_edit.py
@@ -1,42 +1,39 @@
 from tbcml import core
 
-cc = core.CountryCode.from_code("en")
 
-gv = core.GameVersion.from_string_latest("12.3.0", cc)
+class NewForm(core.CustomForm):
+    def __init__(self):
+        super().__init__(form_type=core.CatFormType.FIRST)
 
-apk = core.Apk(gv, cc)
-apk.download()
-apk.extract()
-apk.download_server_files()
+        self.name.set("cool name")
+        self.description.set(["cat that does stuff...", "example cat for tbcml"])
 
-game_data = core.GamePacks.from_apk(apk)
 
-cat_id = 0
+class NewCat(core.CustomCat):
+    def __init__(self):
+        super().__init__(cat_id=0)
 
-cats = core.Cats.from_game_data(game_data, [cat_id])  # avoid loading all cats
-cat = cats.get_cat(cat_id)
-if cat is None:
-    raise ValueError("Cat 0 not found")
+        self.set_form(NewForm())
 
-first_form = cat.get_form(core.CatFormType.FIRST)  # can also do cat.get_form(0)
-if first_form is None:
-    raise ValueError("Cat 0 first form not found")
 
-first_form.name = "Custom Cat"
-first_form.description = ["Custom Cat Description", "Line 2", "Line 3"]
+loader = core.NewModLoader("en", "12.3.0")
+loader.initialize()
 
-mod = core.Mod(
-    "cats_mod",
+
+mod = core.NewMod(
+    "Modded Cat Info Example",
     "fieryhenry",
-    "changes basic cat name",
-    core.Mod.create_mod_id(),
-    "1.0.0",
+    "Changes basic cat first form name and description",
 )
 
-mod_edit = core.ModEdit(["cats", cat_id], cat.to_dict())
-mod.add_mod_edit(mod_edit)
+mod.add_modification(NewCat())
 
-apk.set_app_name("12.3.0")
-apk.set_package_name("jp.co.ponos.battlecatste")
+apk = loader.get_apk()
 
-apk.load_mods([mod], game_data)
+apk.set_app_name("Modded Cat")
+apk.set_package_name("jp.co.ponos.battlecats.moddedcat")
+
+loader.apply(mod)
+
+loader.initialize_adb()
+loader.install_adb(run_game=True)
