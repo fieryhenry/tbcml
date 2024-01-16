c321abc5404119fb337aaece48c2cf9132f3a8b5
fieryhenry <github@henrythomas.me.uk>
2024-01-16 18:28:23 +0000

    add item instead of overwriting speedups

--- examples/itemshop/catfood_item.py
+++ examples/itemshop/catfood_item.py
@@ -10,21 +10,26 @@ class CustomLocalizable(tbcml.Localizable):
 class CatFoodItem(tbcml.ShopItem):
     def __init__(self):
         super().__init__()
-
-        self.shop_id.set(0)
-
         self.count.set(500)
         self.cost.set(1)
         self.gatya_item_id.set(22)
         self.draw_item_value.set(True)
         self.category_name.set("catfood")
+        self.imgcut_rect_id.set(
+            0
+        )  # haven't made my own sprite, so using speed ups sprite
 
 
 class CustomShop(tbcml.ItemShop):
-    def __init__(self):
+    def __init__(self, game_packs: tbcml.GamePacks):
         super().__init__()
+        self.read_data(game_packs)  # add_item needs to know total items, so read data
 
-        self.set_item(CatFoodItem())
+        catfood_item = CatFoodItem()
+
+        self.add_item(
+            catfood_item
+        )  # adds to end of shop, use self.set_item(catfood_item, id) to overwrite existing item
 
 
 loader = tbcml.ModLoader("en", "12.3.0")
@@ -36,10 +41,13 @@ mod = tbcml.Mod(
     "Custom ItemShop", "fieryhenry", "Adds catfood as a custom item shop item"
 )
 
-mod.add_modification(CustomShop())
+mod.add_modification(CustomShop(loader.get_game_packs()))
 mod.add_modification(CustomLocalizable())
 
 apk.set_app_name("Custom Shop")
 apk.set_package_name("jp.co.ponos.battlecats.customshop")
 
 loader.apply(mod)
+
+# loader.initialize_adb()
+# loader.install_adb(run_game=True)
