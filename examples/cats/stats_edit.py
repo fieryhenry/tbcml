4c6a8dfcbb1c85ddb1b38d312a808b534af78c91
fieryhenry <github@henrythomas.me.uk>
2024-01-18 18:49:05 +0000

    stats edit example

---
+++ examples/cats/stats_edit.py
@@ -0,0 +1,45 @@
+import tbcml
+
+
+class NewForm(tbcml.CatForm):
+    def __init__(self, cat_id: int, game_data: tbcml.GamePacks):
+        super().__init__(form_type=tbcml.CatFormType.FIRST)
+        self.read_stats(cat_id, game_data)
+
+        stats = self.get_stats()
+        stats.hp.set(5000)
+        stats.cost.set(0)
+        stats.attack_1_damage.set(8000)
+        stats.speed.set(100)
+        stats.attack_interval.set(0)
+        stats.area_attack.set(True)
+
+
+class NewCat(tbcml.Cat):
+    def __init__(self, game_data: tbcml.GamePacks):
+        super().__init__(cat_id=0)
+
+        self.set_form(NewForm(self.cat_id, game_data))
+
+
+loader = tbcml.ModLoader("en", "12.3.0")
+loader.initialize()
+
+
+mod = tbcml.Mod(
+    "Modded Cat Stats Example",
+    "fieryhenry",
+    "Changes basic cat stats",
+)
+
+mod.add_modification(NewCat(loader.get_game_packs()))
+
+apk = loader.get_apk()
+
+apk.set_app_name("Modded Cat")
+apk.set_package_name("jp.co.ponos.battlecatsen.moddedcat")
+
+loader.apply(mod)
+
+# loader.initialize_adb()
+# loader.install_adb(run_game=True)
