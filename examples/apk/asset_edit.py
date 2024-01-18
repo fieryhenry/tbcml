a46f44bf4eb5ba378a2885a83694c87b0980d978
fieryhenry <github@henrythomas.me.uk>
2024-01-18 18:48:53 +0000

    asset edit example

---
+++ examples/apk/asset_edit.py
@@ -0,0 +1,30 @@
+import tbcml
+
+loader = tbcml.ModLoader("en", "12.3.0")
+loader.initialize()
+
+
+mod = tbcml.Mod(
+    "Modded apk asset Example",
+    "fieryhenry",
+    "Changes user info screen",
+)
+
+local_path = (
+    tbcml.Path(__file__).parent().add("modded_user_info.html")
+)  # modded_user_info.html is in the same folder
+
+mod.add_apk_file(
+    apk_path="assets/user_info_en.html",
+    local_path=local_path,
+)
+
+apk = loader.get_apk()
+
+apk.set_app_name("Modded Asset")
+apk.set_package_name("jp.co.ponos.battlecatsen.moddedasset")
+
+loader.apply(mod)
+
+# loader.initialize_adb()
+# loader.install_adb(run_game=True)
