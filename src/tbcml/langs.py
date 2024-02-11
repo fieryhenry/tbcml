90698bb5f529657c14088271bfcaec0afde330bd
fieryhenry <github@henrythomas.me.uk>
2024-02-11 17:13:49 +0000

    type + get index

--- src/tbcml/langs.py
+++ src/tbcml/langs.py
@@ -1,8 +1,19 @@
 """Contains the Languages enum."""
+
 import enum
+from typing import Literal, Union
+
+LanguageStr = Union[
+    "Language",
+    Literal["de"],
+    Literal["fr"],
+    Literal["it"],
+    Literal["es"],
+    Literal["th"],
+]
 
 
-class Languages(enum.Enum):
+class Language(enum.Enum):
     """Enum for all supported languages in the en version of the game."""
 
     DE = "de"
@@ -12,13 +23,13 @@ class Languages(enum.Enum):
     TH = "th"
 
     @staticmethod
-    def get_all() -> list["Languages"]:
+    def get_all() -> list["Language"]:
         """Gets all languages.
 
         Returns:
             list[Languages]: All languages.
         """
-        return list(Languages)
+        return list(Language)
 
     @staticmethod
     def get_all_strings() -> list[str]:
@@ -27,4 +38,19 @@ class Languages(enum.Enum):
         Returns:
             list[str]: All languages as strings.
         """
-        return [lang.value for lang in Languages.get_all()]
+        return [lang.value for lang in Language.get_all()]
+
+    def get_index(self):
+        # i have no idea what these should be, just guessing.
+        if self == Language.DE:
+            return 0
+        if self == Language.FR:
+            return 1
+        if self == Language.IT:
+            return 2
+        if self == Language.ES:
+            return 3
+        if self == Language.TH:
+            return 4
+
+        raise ValueError("Invalid Language")
