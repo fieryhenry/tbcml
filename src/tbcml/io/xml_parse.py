71081344951017ead4b3d1d1722116072d3e73a2
fieryhenry <github@henrythomas.me.uk>
2024-03-09 14:28:14 +0000

    fix paths starting with manifest

--- src/tbcml/io/xml_parse.py
+++ src/tbcml/io/xml_parse.py
@@ -12,13 +12,18 @@ class XML:
         self.root = ET.fromstring(self.data.to_str())
 
     def get_element(self, path: str) -> Optional[ET.Element]:
-        return self.root.find(path)
+        path = path.replace("manifest", "").strip()
+        path = path.lstrip("/").strip()
+        if not path:
+            return self.root
+        else:
+            return self.root.find(path)
 
     def get_elements(self, path: str) -> list[ET.Element]:
         return self.root.findall(path)
 
     def set_element(self, path: str, value: str):
-        element = self.root.find(path)
+        element = self.get_element(path)
         if element is None:
             raise ValueError("Element not found")
         element.text = value
@@ -37,27 +42,21 @@ class XML:
 
     def set_attribute(self, path: str, attribute: str, value: str):
         attribute = self.get_attribute_name(attribute)
-        if path == "manifest":
-            element = self.root
-        else:
-            element = self.root.find(path)
+        element = self.get_element(path)
         if element is None:
             raise ValueError("Element not found")
         element.set(attribute, value)
 
     def get_attribute(self, path: str, attribute: str) -> Optional[str]:
         attribute = self.get_attribute_name(attribute)
-        if path == "manifest":
-            element = self.root
-        else:
-            element = self.root.find(path)
+        element = self.get_element(path)
         if element is None:
             return None
         return element.get(attribute)
 
     def remove_attribute(self, path: str, attribute: str):
         attribute = self.get_attribute_name(attribute)
-        element = self.root.find(path)
+        element = self.get_element(path)
         if element is None:
             raise ValueError("Element not found")
         element.attrib.pop(attribute)
